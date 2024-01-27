2d6ffa4fe6dc3bf43684356271307d29686cf8ee
osmn_turhn <osmannuri.turhan01@gmail.com>
2024-01-27 14:14:16 +0300

    Add SweetAlert2 library and implement notification functionality

--- src/components/AddTodoComp.tsx
+++ src/components/AddTodoComp.tsx
@@ -2,61 +2,61 @@ import { Box, Button, TextField } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import { useState } from "react";
 
-// const AddTodoComp = ({
-//   addTodo,
-// }: {
-//   addTodo: (text: string) => Promise<void>;
-// }) => {
-//   //   const [text, setText] = useState<string>(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
-//   const [text, setText] = useState("");
+//? 1.Kullanım yolu(Props type tanımlası)
+
+// const AddTodoComp = ({addTodo}:{addTodo:(text:string)=>Promise<void>}) => {
+//   // const [text,setText] = useState<string>("")
+//   const [text,setText] = useState("")//! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
 
 //   const handleClick = () => {
-//     console.log(text);
-//     addTodo(text);
-//     setText("");
-//   };
+//     console.log(text)
+//     addTodo(text)
+//     setText("")
+//   }
 
 //   return (
 //     <Box
-//       sx={{
-//         display: { xs: "block", sm: "flex" },
-//         justifyContent: { xs: "flex-start", sm: "center" },
-//         m: { xs: 1, sm: "auto" },
-//         height: { xs: "120px", sm: "80px" },
-//       }}
-//     >
-//       <TextField
-//         id="outlined-basic"
-//         label="New Todo"
-//         color="success"
-//         sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
-//         variant="outlined"
-//         value={text}
-//         onChange={(e) => setText(e.target.value)}
-//       />
-//       <Button
-//         variant="contained"
-//         color="success"
-//         sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
-//         endIcon={<SaveIcon />}
-//         onClick={handleClick}
+//         sx={{
+//           display: { xs: "block", sm: "flex" },
+//           justifyContent: { xs: "flex-start", sm: "center" },
+//           m: { xs: 1, sm: "auto" },
+//           height: { xs: "120px", sm: "80px" },
+//         }}
 //       >
-//         Save Todo
-//       </Button>
-//     </Box>
-//   );
-// };
+//         <TextField
+//           id="outlined-basic"
+//           label="New Todo"
+//           color="success"
+//           sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+//           variant="outlined"
+//           value={text}
+//           onChange={(e)=>setText(e.target.value)}
+//         />
+//         <Button
+//           variant="contained"
+//           color="success"
+//           sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+//           endIcon={<SaveIcon />}
+//           onClick={handleClick}
+//         >
+//           Save Todo
+//         </Button>
+//       </Box>
+//   )
+// }
 
-// export default AddTodoComp;
+// export default AddTodoComp
+
+//? 2.Kullanım yolu(Props type tanımlası)
 
 interface IAddTodoComp {
-  //   addTodo: (text: string) => Promise<void>;
+  // addTodo:(text:string)=>Promise<void>;
   addTodo: AddFn;
 }
 
 const AddTodoComp = ({ addTodo }: IAddTodoComp) => {
-  //   const [text, setText] = useState<string>(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
-  const [text, setText] = useState("");
+  // const [text,setText] = useState<string>("")
+  const [text, setText] = useState(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
 
   const handleClick = () => {
     console.log(text);
@@ -81,6 +81,7 @@ const AddTodoComp = ({ addTodo }: IAddTodoComp) => {
         variant="outlined"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        inputProps={{ maxLength: 40 }}
       />
       <Button
         variant="contained"
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,12 @@
 //! .d.ts uzantılı dosyaları typescript dosyası olarak tanımlamak için kullanılır.
 
+interface ITodoType {
+  id: string | number;
+  isDone: boolean;
+  task: string;
+  todo?: string;
+}
+
 type AddFn = (text: string) => Promise<void>;
+type ToggleFn = (todo: ITodoType) => Promise<void>;
+type DeleteFn = (id: string | number) => Promise<void>;
