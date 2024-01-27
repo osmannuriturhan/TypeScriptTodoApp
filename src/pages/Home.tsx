eb30dfccfa2600c65a8bee2b03aa92e31a5e3678
osmn_turhn <osmannuri.turhan01@gmail.com>
2024-01-27 12:49:43 +0300

    Refactor AddTodoComp and Home components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import AddTodoComp from "../components/AddTodoComp";
 import TodoList from "../components/TodoList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface ITodoType {
@@ -12,18 +12,37 @@ interface ITodoType {
   todo?: string;
 }
 
+const url: string = import.meta.env.VITE_BASE_URL;
+
 const Home = () => {
-  const [todos, setTodos] = useState([]);
+  //   const [todos, setTodos] = ((useState < []) as ITodoType) > []; //* type tanımlaması 1.yol fakat çok fazla tercih edilmiyor.
+  //   const [todos, setTodos] = useState<Array<ITodoType>>([]); //* type tanımlaması 2.yol
+  const [todos, setTodos] = useState<ITodoType[]>([]); //* type tanımlaması 3.yol en çok kullanılan
 
   const getTodos = async () => {
     try {
-      const { data } = await axios("");
-    } catch (error) {}
+      const { data } = await axios.get<ITodoType[]>(url);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
     <Container>
-      <Typography>TodoApp with TypeScript</Typography>
+      <Typography
+        color="error"
+        align="center"
+        variant="h2"
+        component="h1"
+        onClick={() => setTodos([{ id: 1, isDone: false, task: "asdasd" }])}
+      >
+        TodoApp with TypeScript
+      </Typography>
       <AddTodoComp />
       <TodoList />
     </Container>
